197a6b765e815684c6d05c9340579dfc3ee16b64
Adrian <adrian.capchaq@gmail.com>
2023-01-15 21:08:35 -0500

    creando estructura del proyecto

---
+++ Test.py
@@ -0,0 +1,15 @@
+"""
+
+Diseñar un programa que gestione una agenda telefónica con los nombres y los números de contacto de los clientes de una organización. El programa incorporará que presentará el siguiente menú:
+Agenda telefónica de ABC SA
+==========================
+1 - Consultar un número de contacto.
+2 - Añadir un número de contacto.
+3 - Eliminar un número de contacto.
+4 - Crear la agenda.
+0 - Terminar. 
+
+
+"""
+
+
