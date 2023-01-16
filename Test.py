b02f5b6bf71c4b0a851ec4aafcfff64346be378d
Adrian <adrian.capchaq@gmail.com>
2023-01-15 23:28:23 -0500

    creando agenda base

--- Test.py
+++ Test.py
@@ -1,3 +1,5 @@
+# importando metodos
+
 """
 
 Diseñar un programa que gestione una agenda telefónica con los nombres y los números de contacto de los clientes de una organización. El programa incorporará que presentará el siguiente menú:
@@ -11,5 +13,10 @@ Agenda telefónica de ABC SA
 
 
 """
+# Contacto base
+agenda=[]
+contacto={'id':'1','nombre': 'Juan','apellido':'Perez', 'telefono': '956123456'}
+agenda.append(contacto)
+print(agenda)
 
 
