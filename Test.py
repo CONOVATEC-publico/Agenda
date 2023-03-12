cf9b8c2da9f649ce5e278045b94d6a3af91304de
ETERRAZAS21PE <eterrazas.met.pe21@gmail.com>
2023-03-12 18:53:44 -0500

    Actualizando Test.py

--- Test.py
+++ Test.py
@@ -25,14 +25,14 @@ agenda.append(contacto)
 
 contacto = {'id': 2,'nombre': 'Pedro','apellido':'Ramirez', 'telefono': '965876541'}
 agenda.append(contacto)
-for x in range(3,20):
-    contacto = {'id': x,'nombre': 'Miguel' + str(x),'apellido':'Grau' + str(x), 'telefono': x}
+for x in range(3,7):
+    contacto = {'id': x,'nombre': 'Miguel_' + str(x),'apellido':'Grau_' + str(x), 'telefono': x+925059890}
     agenda.append(contacto)
 
 # print(agenda)
 ventana = Tk()
 ventana.title("Agenda Telefónica")
 
-agregar(agenda,"Isacc","Newton","988888821")
+agregar(agenda,"Gauss","Jordano","988888821")
 
 App(agenda, ventana) #mostrarAgenda(agenda, ventana)
