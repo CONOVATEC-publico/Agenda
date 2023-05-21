4cbdc1eaaad56c76cbe6326ad3f5af8a28fe8096
ETERRAZAS21PE <eterrazas.met.pe21@gmail.com>
2023-05-21 00:04:27 -0500

    Definiendo el método EliminarVentana()

--- vistas.py
+++ vistas.py
@@ -104,7 +104,7 @@ class App:
 
         ventana.mainloop()
 
-    def eliminarContactoAgenda(self):
+    def VentanaEliminar(self, Button_SI, Button_NO):
         # --------------Ventana Eliminar-----------------
         ventana_eliminar = tkinter.Tk()
         ventana_eliminar.title("               Eliminar               ")
@@ -114,23 +114,33 @@ class App:
         # --------Etiqueta de la ventana eliminar--------
         Etiqueta_VE1 = tkinter.Label(ventana_eliminar, text=" ").pack()
         Etiqueta_VE2 = tkinter.Label(ventana_eliminar, text="¿Está seguro que quiere eliminar este contacto").pack()
-        Etiqueta_VE3 = tkinter.Label(ventana_eliminar, text="¿de su agenda telefónica?").pack()
+        Etiqueta_VE3 = tkinter.Label(ventana_eliminar, text="de su agenda telefónica?").pack()
 
         # --------------------Botones--------------------
         Button_SI = Button(ventana_eliminar, text="Si")
-        #Button_SI[COMMAND] = eliminarContacto(index, self.agenda)
+        Button_SI[COMMAND] = self.eliminarContactoAgenda
         Button_SI.place(x=60, y=80, width=30)
+
+        Button_NO = Button(ventana_eliminar, text="No")
+        Button_NO[COMMAND] = self.agenda
+        Button_NO.place(x=245, y=80, width=30)
 
         ventana_eliminar.mainloop()
         # -----------------------------------------------
-        selection = self.treeview.selection()
+    
+    def eliminarContactoAgenda(self):
 
+        selection = self.treeview.selection()
+    
         if len(selection) > 0:
-            id = selection[0]
-            index = self.treeview.item(id, "text")
-            Button_SI[COMMAND] = eliminarContacto(index, self.agenda)
-            eliminarContacto(index, self.agenda)
-            self.rellenaTreeview()
+            #self.VentanaEliminar(Button_SI=TRUE, Button_NO=FALSE)
+            if self.VentanaEliminar(Button_SI=TRUE, Button_NO=FALSE) == True:
+                id = selection[0]
+                index = self.treeview.item(id, "text")
+                eliminarContacto(index, self.agenda)
+                self.rellenaTreeview()
+            else:
+                self.VentanaEliminar(Button_SI=FALSE, Button_NO=TRUE)
 
     def rellenaTreeview(self, idFiltro='', nombreFiltro='', telefonoFiltro=''):
         self.treeview.delete(*self.treeview.get_children())
